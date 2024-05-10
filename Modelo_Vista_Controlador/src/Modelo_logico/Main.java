f344c7366313580a5586a4f477777325a573b856
“Augusto <aperrone@myuax.com>
2024-05-10 20:30:24 +0200

    fix - main

--- Modelo_Vista_Controlador/src/Modelo_logico/Main.java
+++ Modelo_Vista_Controlador/src/Modelo_logico/Main.java
@@ -1,6 +1,7 @@
 package Modelo_Vista_Controlador.src.Modelo_logico;
 
-import Modelo_Vista_Controlador.src.Modelo_logico.AnalizadoresSintácticos.HandlerFinalDeSentenciasSyntax;
+import Modelo_Vista_Controlador.src.Modelo_logico.AnalizadoresLéxicos.HandlerPalabrasNoReservadas;
+import Modelo_Vista_Controlador.src.Modelo_logico.AnalizadoresSintácticos.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,10 +12,23 @@ public class Main {
         CountUp cuentaArriba = new CountUp();
         cuentaArriba.contadorHaciaArriba();
 
-        HandlerFinalDeSentenciasSyntax handlerFinalDeSentenciasSyntax = new HandlerFinalDeSentenciasSyntax();
-        handlerFinalDeSentenciasSyntax.proceso(cuentaAtras.toString());
+        HandlerEspaciosEnBlancoSyntax handlerEspacios = new HandlerEspaciosEnBlancoSyntax();
+        HandlerFinalDeSentenciasSyntax handlerFinal = new HandlerFinalDeSentenciasSyntax();
+        HandlerForSyntax handlerFor = new HandlerForSyntax();
+        HandlerPalabrasNoReservadas handlerPalabras = new HandlerPalabrasNoReservadas();
+        HandlerPalabrasReservadasSyntax handlerPalabrasReservadas = new HandlerPalabrasReservadasSyntax();
+        HandlerWhileSyntax handlerWhile = new HandlerWhileSyntax();
+        System.out.println("-------------------------------------------------");
 
+        handlerEspacios.setSiguiente(handlerFinal);
+        handlerFinal.setSiguiente(handlerFor);
+        handlerFor.setSiguiente(handlerPalabras);
+        handlerPalabras.setSiguiente(handlerPalabrasReservadas);
+        handlerPalabrasReservadas.setSiguiente(handlerWhile);
 
+        System.out.println("-------------------------------------------------");
+
+        handlerEspacios.proceso(cuentaAtras.toString());
 
 
     }
